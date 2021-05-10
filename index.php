ac2998d78ca9c58d38a5b3bd3db22003145a9535
jsonukr55 <55572908+jsonukr55@users.noreply.github.com>
2021-05-10 20:23:26 +0530

    about page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/skin-1.css">
     <link rel="stylesheet" type="text/css" href="css/responsive.css">
-    
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
 </head>
 <body>
@@ -76,11 +76,32 @@
         </div>
     </section>
 
-    <section class="about-section section">
-        <div class="container">
+    <section class="about-section section ">
+        <div class="container ">
             <div class="row">
                 <div class="section-title">
                     <h2 data-heading="main info">About Me</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="about-img ">
+                    <div class="img-box inner-shadow">
+                        <img src="asset/propic.png" class="outer-shadow" alt="profile pic">
+                    </div>
+                    <div class="social-links">
+                        <a href="#" class="outer-shadow hover-in-shadow" ><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="outer-shadow hover-in-shadow" ><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="outer-shadow hover-in-shadow" ><i class="fab fa-linkedin-in"></i></a>
+                        <a href="#" class="outer-shadow hover-in-shadow" ><i class="fab fa-github"></i></a>
+
+
+                    </div>
+                </div>
+                <div class="about-info">
+                    <p><span>Hi! My name is Dhananajay kr. Gupta.</span>  I'm student of B.tech Computer Science Enggneering final year.
+                    This website is my final year project as well as portfolio of my work.  </p>
+                    <a href="asset/Resume.pdf" class="btn-1 outer-shadow hover-in-shadow" target="_blank">Download CV</a>
+                    <a href="#" class="btn-1 outer-shadow hover-in-shadow">Hire Me</a>
                 </div>
             </div>
         </div>
