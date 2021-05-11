e796290c993f76cb7440f1d2a58b41bbf1c5d521
jsonukr55 <55572908+jsonukr55@users.noreply.github.com>
2021-05-12 01:02:38 +0530

    complete about section

--- index.php
+++ index.php
@@ -104,9 +104,163 @@
                     <a href="#" class="btn-1 outer-shadow hover-in-shadow">Hire Me</a>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="about-tabs">
+                    <span class="tab-item outer-shadow active" data-target=".skills">skills</span>
+                    <span class="tab-item" data-target=".experience">experience</span>
+                    <span class="tab-item" data-target=".education">education</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="skills tab-content active">
+                    <div class="row">
+                        <div class="skill-item">
+                            <p>Html</p>
+                            <div class="progress inner-shadow">
+                                <div class="progress-bar" style="width: calc(80% - 14px);">
+                                    <span>80%</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="skill-item">
+                            <p>JavaScript</p>
+                            <div class="progress inner-shadow">
+                                <div class="progress-bar" style="width: calc(75% - 14px);">
+                                    <span>75%</span>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="skill-item">
+                            <p>Css</p>
+                            <div class="progress inner-shadow">
+                                <div class="progress-bar" style="width: calc(70% - 14px);">
+                                    <span>70%</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="skill-item">
+                            <p>Python</p>
+                            <div class="progress inner-shadow">
+                                <div class="progress-bar" style="width: calc(80% - 14px);">
+                                    <span>80%</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="skill-item">
+                            <p>Java</p>
+                            <div class="progress inner-shadow">
+                                <div class="progress-bar" style="width: calc(50% - 14px);">
+                                    <span>50%</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="skill-item">
+                            <p>Django</p>
+                            <div class="progress inner-shadow">
+                                <div class="progress-bar" style="width: calc(60% - 14px);">
+                                    <span>60%</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="experience tab-content">
+                    <div class="row">
+                        <div class="timeline">
+                            <div class="row">
+                                <div class="timeline-item">
+                                    <div class="timeline-item-inner outer-shadow">
+                                        <i class="fas fa-briefcase icon"></i>
+                                        <span>Feb 2021 – Apr 2021</span>
+                                            <h3>Developer Intern</h3>
+                                            <h4>E-contruct design and build pvt. ltd.</h4>
+                                            <p>Planned website development, converting mockups into usable 
+                                                web presence with HTML, CSS, JavaScript, coding. Worked as a 
+                                                Trainee Developer. Mainly worked on Payment Gateway and API 
+                                                Integration with in-house developed software.
+                                                </p>
+                                    </div>
+                                </div>
+                                <div class="timeline-item">
+                                    <div class="timeline-item-inner outer-shadow">
+                                        <i class="fas fa-briefcase icon"></i>
+                                        <span>Oct 2020 – Oct 2020</span>
+                                            <h3>College Project</h3>
+                                            <h4>Blog Site</h4>
+                                            <p>Planned website development with Python, Django, HTML, CSS, 
+                                                JavaScript, coding. This Project was a Blog Site where a user can 
+                                                register/login and make their own blog posts and share. A user can 
+                                                post in any format i.e., Images, Text and Video.
+                                                </p>
+                                    </div>
+                                </div>
+                                <div class="timeline-item">
+                                    <div class="timeline-item-inner outer-shadow">
+                                        <i class="fas fa-briefcase icon"></i>
+                                        <span>Jan 2021 – Feb 2021</span>
+                                            <h3>College Project</h3>
+                                            <h4>Number Plate Detection</h4>
+                                            <p>This project aims to recognize license number plates. In order to 
+                                                detect license number plates, we will use OpenCV to identify number 
+                                                plates and Python Pytesseract to extract characters and digits from 
+                                                the number plates. Images, Text and Video.
+                                                </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="education tab-content">
+                    <div class="row">
+                        <div class="timeline">
+                            <div class="row">
+                                <div class="timeline-item">
+                                    <div class="timeline-item-inner outer-shadow">
+                                        <i class="fas fa-graduation-cap icon"></i>
+                                        <span>Aug 2017 – Present</span>
+                                            <h3>B.Tech CSE</h3>
+                                            <h4>NSHM Knowledge Campus, Durgapur</h4>
+                                            <!-- <p>Planned website development, converting mockups into usable 
+                                                web presence with HTML, CSS, JavaScript, coding. Worked as a 
+                                                Trainee Developer. Mainly worked on Payment Gateway and API 
+                                                Integration with in-house developed software.
+                                                </p> -->
+                                    </div>
+                                </div>
+                                <div class="timeline-item">
+                                    <div class="timeline-item-inner outer-shadow">
+                                        <i class="fas fa-graduation-cap icon"></i>
+                                        <span>Apr 2016 – Mar 2017</span>
+                                            <h3>Intermediate(10+2)</h3>
+                                            <h4>JSLM DAV College, Bhaga</h4>
+                                            <!-- <p>Planned website development with Python, Django, HTML, CSS, 
+                                                JavaScript, coding. This Project was a Blog Site where a user can 
+                                                register/login and make their own blog posts and share. A user can 
+                                                post in any format i.e., Images, Text and Video.
+                                                </p> -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
 
     </section>
+
+<script src="JavaScript/index.js"></script>
 
 </body>
 </html>
