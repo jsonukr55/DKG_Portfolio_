fbd629ff6faf56aad370c21b3641a552df800680
jsonukr55 <55572908+jsonukr55@users.noreply.github.com>
2021-06-02 01:44:04 +0530

    changes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,71 @@
+// nav
+
+(()=>{
+    const hamburgerBtn = document.querySelector(".hamburger-btn"),
+    navMenu = document.querySelector(".nav-menu"),
+    closeNavBtn = navMenu.querySelector(".close-nav-menu");
+
+    hamburgerBtn.addEventListener("click", showNavMenu);
+    closeNavBtn.addEventListener("click", hideNavMenu);
+
+    function showNavMenu(){
+        navMenu.classList.add("open");
+        // bodyScrollingToggle();
+    }
+    function hideNavMenu(){
+        navMenu.classList.remove("open");
+        fadeOutEffect();
+        // bodyScrollingToggle();
+    }
+    function fadeOutEffect(){
+        document.querySelector(".fade-out-effect").classList.add("active");
+        setTimeout(()=>{
+            document.querySelector(".fade-out-effect").classList.remove("active");
+
+        },300)
+    }
+    document.addEventListener("click", (event) => {
+        if(event.target.classList.contains('link-items')){
+            if(event.target.hash !==""){
+                event.preventDefault();
+                const hash = event.target.hash;
+                // console.log(hash);
+                document.querySelector(".section.active").classList.add("hide");
+                document.querySelector(".section.active").classList.remove("active");
+
+                document.querySelector(hash).classList.add("active");
+                document.querySelector(hash).classList.remove("hide");
+
+                navMenu.querySelector(".active").classList.add("outer-shadow","hover-in-shadow");
+                navMenu.querySelector(".active").classList.remove("active","inner-shadow");
+
+                if(navMenu.classList.contains("open")){
+                    event.target.classList.add("active","inner-shadow");
+                    event.target.classList.remove("outer-shadow","hover-in-shadow");
+                    hideNavMenu();
+                }
+                else{
+                    let navItems = navMenu.querySelectorAll(".link-items");
+                    navItems.forEach((item) =>{
+                        if(hash === item.hash){
+                            item.classList.add("active", "inner-shadow");
+                            item.classList.remove("outer-shadow", "hover-in-shadow");
+                        }
+                    })
+                    fadeOutEffect();
+                }
+            }
+        }
+    })
+
+
+})();
+
+
+
+
+
+// about
 (() =>{
     const aboutSection = document.querySelector(".about-section"),
     tabsContainer = document.querySelector(".about-tabs"); 
@@ -13,3 +81,13 @@
     })
 })();
 
+// hide
+
+(() =>{
+    const section = document.querySelectorAll(".section");
+    section.forEach((section)=>{
+        if(!section.classList.contains("active")){
+            section.classList.add("hide");
+        }
+    })
+})();
